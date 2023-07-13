6517526cc50c928fa478c5a1bbbc4c8e0df9d1dc
Javier Caballero <caballeroperezjavier@gmail.com>
2023-07-13 08:03:10 -0500

    se añade footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './footer.css';
+import logo from '../../utilities/Logo_footer.png'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
+import logoWhatsapp from '../../utilities/WhatsApp.png'
+import logoInstagram from '../../utilities/instagram.png'
+import logoCorreo from '../../utilities/correo.png'
+import logoTiktok from '../../utilities/tiktok.png'
+import logoFacebook from '../../utilities/facebook.png'
+
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+      <Row className='row'>
+        <Col className='col'>
+          <Image className='logo' src={logo}/>
+        </Col>
+        <Col className='redes'>
+          <Row className='contRedes'>
+                <Col className='cajaRedes'>
+                <Image className='iconRedes' src={logoWhatsapp} />
+               <p className= 'pRedes'> WhatsApp</p>
+                </Col>
+                <Col className='cajaRedes' >
+                <Image className='iconRedes' src={logoCorreo} />
+                <p className= 'pRedes'>Correo </p>
+                </Col>
+                <Col className='cajaRedes' >
+                <Image className='iconRedes' src={logoInstagram} />
+                <p className= 'pRedes'>Instagram </p>
+                </Col>
+                <Col className='cajaRedes' >
+                <Image className='iconRedes' src={logoFacebook} />
+                <p className= 'pRedes'>Facebook </p>
+                </Col>
+                <Col className='cajaRedes' >
+                <Image className='iconRedes' src={logoTiktok} />
+               <p className= 'pRedes'>Tiktok </p>
+                </Col>
+               
+          </Row>
+        </Col>
+        <Col className='listPag hidden md:block'>
+            <Row >
+            <p>Inicio</p>
+            </Row>
+            <Row >
+            <p>Sobre Nosotras</p>
+            </Row>
+            <Row >
+            <p>Conócenos</p>
+            </Row>
+            <Row>
+            <p>Contáctanos</p>
+            </Row>
+          </Col>
+      </Row>
+    </footer>
+       
+
+    </>
+  )
+}
+
+export default Footer
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+        'tablet': '768px',  // Ejemplo de breakpoint para tablet
+        'mobile': '480px',  // Ejemplo de breakpoint para dispositivos móviles
+    },
+  },
+  plugins: [],
+}
+
