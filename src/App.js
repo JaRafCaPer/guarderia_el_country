a6b66c3dc64f483035848c77fbe1d3cebd11ee1a
Javier Caballero <caballeroperezjavier@gmail.com>
2023-07-13 15:19:44 -0500

    Se crea la primera seccion de home sin efectos en la imagen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Footer from "./components/footer/footer.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import Home from "./components/Home/Home.jsx";
 function App() {
   return (
   <div className='App'>
@@ -11,6 +12,7 @@ function App() {
         <Routes>
 
         </Routes>
+        <Home/>
         <Footer/>
       </BrowserRouter>
     
