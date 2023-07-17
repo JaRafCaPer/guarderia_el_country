8f8ed129a027561a753b63684b283cc1272ea264
Jhoceliz Figueroa <jhocelizf@gmail.com>
2023-07-17 10:01:31 -0300

    little changes nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -38,7 +38,6 @@ const NavBar = () => {
     }, []);
     return (
         <>
-
             <div className="nav">
                 <div className="logo">
                     <img src={logoi} alt="" className="lDog" />
@@ -46,39 +45,43 @@ const NavBar = () => {
                 <div className="logo">
                     <img src={logo} alt="" className="logoN" />
                 </div>
-                <div className="container" >
-                    <ul style={{ display: hamburgerOpen ? "inline" : "none" }}>
-                        <li className="navLink" onClick={() => handleClick("inicio")}
-                            style={{ color: color === "inicio" ? "#4F6C7C" : "black" }}>Inicio</li>
-                        <li className="navLink" onClick={() => handleClick("sobre")}
-                            style={{ color: color === "sobre" ? "#4F6C7C" : "black" }}>Sobre Nosotras</li>
-                        <li className="navLink" onClick={() => handleClick("conocenos")}
-                            style={{ color: color === "conocenos" ? "#4F6C7C" : "black" }}>Conócenos</li>
-                        <li className="navLink" onClick={() => handleClick("contactanos")}
-                            style={{ color: color === "contactanos" ? "#4F6C7C" : "black" }}>Contáctanos</li>
-
-
-                        <li className="linkRedes">
-                            <img src={WhatsApp} alt="Descripción de la imagen" className="iconRedes " />
-                            WhatsApp
-                        </li>
-                        <li className="linkRedes">
-                            <img src={Instagram} alt="Descripción de la imagen" className="iconRedes "/>
-                            Intagram
-                        </li> 
-                        <li className="linkRedes">
-                            <img src={Facebook} alt="Descripción de la imagen" className="iconRedes "/>
-                            Facebook
-                        </li>
-                        <li className="linkRedes">
-                            <img src={Tiktok} alt="Descripción de la imagen" className="iconRedes "/>
-                            Tiktok
-                        </li>
-                        <li className="linkRedes">
-                            <img src={Mail} alt="Descripción de la imagen" className="iconRedes "/>
-                            Mail
-                        </li>
-                    </ul>
+                <div className="containerDad" >
+                    <div className="container"  style={{ display: hamburgerOpen ? "inline" : "none" }}>
+                        <ul >
+                            <li className="navLink" onClick={() => handleClick("inicio")}
+                                style={{ color: color === "inicio" ? "#4F6C7C" : "black" }}>Inicio</li>
+                            <li className="navLink" onClick={() => handleClick("sobre")}
+                                style={{ color: color === "sobre" ? "#4F6C7C" : "black" }}>Sobre Nosotras</li>
+                            <li className="navLink" onClick={() => handleClick("conocenos")}
+                                style={{ color: color === "conocenos" ? "#4F6C7C" : "black" }}>Conócenos</li>
+                            <li className="navLink" onClick={() => handleClick("contactanos")}
+                                style={{ color: color === "contactanos" ? "#4F6C7C" : "black" }}>Contáctanos</li>
+                        </ul>
+                    </div>
+                    <div className="redesContainer" style={{ display: hamburgerOpen ? "inline" : "none" }}>
+                        <ul>
+                            <li className="linkRedes">
+                                <img src={WhatsApp} alt="Descripción de la imagen" className="iconRedes " />
+                                WhatsApp
+                            </li>
+                            <li className="linkRedes">
+                                <img src={Instagram} alt="Descripción de la imagen" className="iconRedes " />
+                                Intagram
+                            </li>
+                            <li className="linkRedes">
+                                <img src={Facebook} alt="Descripción de la imagen" className="iconRedes " />
+                                Facebook
+                            </li>
+                            <li className="linkRedes">
+                                <img src={Tiktok} alt="Descripción de la imagen" className="iconRedes " />
+                                Tiktok
+                            </li>
+                            <li className="linkRedes">
+                                <img src={Mail} alt="Descripción de la imagen" className="iconRedes " />
+                                Mail
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="hamburger" onClick={toggleHamburger}>
                     <Burger />
