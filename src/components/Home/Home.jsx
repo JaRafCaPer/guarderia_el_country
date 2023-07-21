55e5b0cb4c5c2dc124f20f277a405c538e089b33
Javier Caballero <caballeroperezjavier@gmail.com>
2023-07-21 08:27:38 -0500

    se borran los otros componenetes para poder observar mejor el home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,30 +12,30 @@ const Home = () => {
     <>
     <div className='bg'>
         <div className='mainContainer'>
-            <Row className='sec1Cont'>
-                <Row className='innerCont'>
-                        <Col className='col1'>
-                            <Row className='texto'>
+            <div className='sec1Cont'>
+                <div className='innerCont'>
+                        <div className='col1'>
+                            <div className='texto'>
                                 
                                     <h1> El Country </h1>
                                     <p className='pText'>Alojamiento para perros compuesto por dos educadoras caninas y tres perritas las cuales acompañarán a tu pequeño mientras debas ausentarte.
                                     Conocenos e infórmate sobre nuestro servicio brindado con la mejor calidad y cuidado para tu perro, ¡se sentirá como en casa en todo momento!</p>
-                            </Row>
-                            <Row>
+                            </div>
+                            <div>
                                 <button className="button">
                                     Contactanos
                                 </button>
-                            </Row>
+                            </div>
 
-                        </Col>
-                        <Col className='col2'>
+                        </div>
+                        <div className='col2'>
                             <Image className='imgPerro' src={perroNaranja}/>
-                        </Col>
-                </Row>
-            </Row>
-            <Row className='testCards'>
+                        </div>
+                </div>
+            </div>
+            <div className='testCards'>
                 
-            </Row>
+            </div>
         </div>
     </div>
     </>
