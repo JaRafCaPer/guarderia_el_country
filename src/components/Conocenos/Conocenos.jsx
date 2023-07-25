de4a50f1fe496b60ffaa7e7b1e383166d645e13f
Javier Caballero <caballeroperezjavier@gmail.com>
2023-07-25 06:58:17 -0500

    Trabajos en el carousel de fotos

--- src/components/Conocenos/Conocenos.jsx
+++ src/components/Conocenos/Conocenos.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Conocenos.css'
-import { Image } from 'react-bootstrap'
+import { Image, Carousel } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card';
 import perroAzul from '../../assets/images/Perro - azul.svg'
 import CarouselReviews from '../CarouselReviews/CarouselReviews'
+import CarouselMid from '../../assets/images/Pampa-Bubba-Petit (42) 1.jpg'
+import CarouselIni from '../../assets/images/Pampa-Bubba-Petit (34) 1.jpg'
+import CarouselSec from '../../assets/images/Pampa-Bubba-Petit (27) 1.jpg'
+import CarouselCua from '../../assets/images/Pampa-Bubba-Petit (24) 1.jpg'
+import CarouselQui from '../../assets/images/Pampa-Bubba-Petit (35) 1.jpg'
 
 
 const Conocenos = () => {
@@ -14,12 +20,51 @@ const Conocenos = () => {
                     <h1 className='nosotrasTitle'> Nosotras</h1>
                 </div>
                 <div className='carrouselFotosContainer'>
-                    <div className='carrouselFotos'>
-                        <Image/>
-                        <Image/>
-                        <Image/>
-                        <Image/>
-                    </div>
+                    <Carousel data-bs-theme="while" className="slide" data-bs-interval="1000">
+                        <Carousel.Item>
+                            <div className='carrouselFotos'>
+                                <div className='colCar'>
+                                        <Card className='card'>
+                                            <div className="image-container">
+                                                <Card.Img  src={CarouselIni} className='imgCar' />
+                                            </div>
+                                        </Card>
+                                </div>
+                                <div className='colCar'>
+                                        <Card className='card'>
+                                            <div className="image-container">
+                                                <Card.Img  src={CarouselSec} className='imgCar' />
+                                            </div>
+                                        </Card>
+                                </div>
+                                <div className='colCar'>
+                                        <Card className='card1'>
+                                            <div className="image-container">
+                                                <Card.Img  src={CarouselMid} className='imgCar' />
+                                            
+                                            </div>
+
+                                        </Card>
+                                </div>
+                                <div className='colCar'>
+                                        <Card className='card'>
+                                            <div className="image-container">
+                                                <Card.Img  src={CarouselCua} className='imgCar' />
+                                            
+                                            </div>
+
+                                        </Card>
+                                </div>
+                                <div className='colCar'>
+                                        <Card className='card'>
+                                            <div className="image-container">
+                                                <Card.Img  src={CarouselQui} className='imgCar' />
+                                            </div>
+                                        </Card>
+                                </div>
+                            </div>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
                 <div className='carrouselReviews'>
                         <CarouselReviews/>
